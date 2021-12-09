8b22f56d5506c323451247961f58380d67271cac
g20919 <95402812+g20919@users.noreply.github.com>
2021-12-09 22:52:19 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,9 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
   if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
-  } else {
     myImage.setAttribute ('src','images/newfirefox.png');
+  } else {
+    myImage.setAttribute ('src','images/firefox2.png');
   }
 }
 
